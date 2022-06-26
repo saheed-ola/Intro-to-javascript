30c75b53c62bdae2630e49ed27fea311bde71c47
saheed-ola <abubakar.saheed50@gmail.com>
2022-06-26 13:38:31 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var MyName = ('Abubakar Saheed Olamide')
+var MyHeight = ('6 ft')
+var MyCountry = ('Nigeria')
+
+document.write('My name is '+ MyName)
+document.write('<br> My height is '+ MyHeight)
+document.write("<br>I am from "+ MyCountry)
